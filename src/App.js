5e16dc43859ae3ba810657db9d921fcbbbc93d3a
your Shubinder Virk <shubinder92@gmail.com>
2020-06-19 18:52:13 +0100

    Added A react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavToolBar from "./Containers/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <NavToolBar />
+
+        <Switch>
+          <Route path="/">
+            <Intro />
+          </Route>
+
+          <Route path="/">
+            <Introduction />
+          </Route>
+
+          <Route path="/skills">
+            <ManagingBooks />
+          </Route>
+
+          <Route path="/projects">
+            <Projects />
+          </Route>
+
+          <Route path="/">
+            <Test />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Containers/Navbar/Navbar.js
+++ src/Containers/Navbar/Navbar.js
@@ -6,62 +6,50 @@ import {
   FormControl,
   Button,
   Nav
-
 } from "react-bootstrap";
 
-
 const NavToolBar = () => (
-    // let navRoutes = [
-    //   "SUMMARY",
-    //   "SKILLS",
-    //   "PROJECTS",
-    //   "ARTICLES",
-    //   "GET IN TOUCH"
-    // ];
-  
-      <Container>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">
-          Shubinder Virk
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#/">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown
-                title="Dropdown"
-                id="basic-nav-dropdown"
-              >
-                <NavDropdown.Item href="/skills">
-                  Skills
-                </NavDropdown.Item>
-                <NavDropdown.Item href="projects">
-                Work Experience
-                </NavDropdown.Item>
-                <NavDropdown.Item href="design">
-                 Design
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Play CoronaVirus Game Run
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
-              />
-              <Button variant="outline-success">
-                Search
-              </Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
-      </Container>
-  );
-  
-  export default NavToolBar;
-  
+  // let navRoutes = [
+  //   "SUMMARY",
+  //   "SKILLS",
+  //   "PROJECTS",
+  //   "ARTICLES",
+  //   "GET IN TOUCH"
+  // ];
+
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">
+        ASK Prestige
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#/">Home</Nav.Link>
+        <Nav.Link href="#link">Make Booking</Nav.Link>
+        <Nav.Link href="#link">Sign In</Nav.Link>
+        <Nav.Link href="#link">Make Booking</Nav.Link>
+        
+        <NavDropdown
+          title="Dropdown"
+          id="basic-nav-dropdown"
+        >
+          <NavDropdown.Item href="/skills">
+            Skills
+          </NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      <Form inline>
+        <FormControl
+          type="text"
+          placeholder="Search"
+          className="mr-sm-2"
+        />
+        <Button variant="outline-success">
+          Search
+        </Button>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default NavToolBar;
