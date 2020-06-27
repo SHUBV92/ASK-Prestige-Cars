61436845d400d7d46d88e2fa442f98dbbb6afd89
your Shubinder Virk <shubinder92@gmail.com>
2020-06-27 16:31:47 +0100

    A user can add Booking Details

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 
-const CardView = () => (
+const CardView = (props) => (
   <div>
     {/* <Image>
             <img 
                 src={require(`../../assets/ /${image}`)}
 
         </Image> */}
+    {console.log("Props", props)}
+
 
     <Card>
-      <Card.Header>Featured</Card.Header>
+      <Card.Header>Bookings</Card.Header>
       <Card.Body>
-        <Card.Title>Name: Shubs</Card.Title>
+        <Card.Title>Name: {props.booking.name}</Card.Title>
         <Card.Text>
-          Travel Date: 25th Aug - 25th Sep 2020
+          Travel Date: {props.booking.date}
         </Card.Text>
         <Button variant="primary">
           Manage Booking
--- src/Containers/ManageBookings/ManageBookings.jsx
+++ src/Containers/ManageBookings/ManageBookings.jsx
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import CardView from "../../Components/Card/Card.jsx";
 
 const ManageBookings = () => {
-  const handlechange = (event) => {};
+  const [name, setName] = useState(" ");
+  const [lastName, setLastName] = useState(" ");
+  const [date, setDate] = useState(" ");
+  const [bookings, setBookings] = useState([]);
+
+  const handleNameChange = (event) => {
+    console.log("Name: ", event.target.value);
+    setName(event.target.value);
+  };
+
+  const handleLastChange = (event) => {
+    console.log(
+      "Last Name: ",
+      event.target.value
+    );
+    setLastName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    console.log("Date: ", event.target.value);
+    setDate(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const addBookings = () => {
+    console.log("A", bookings)
+    setBookings(
+      bookings.concat({
+        name: name,
+        lastName: lastName,
+        date: date,
+      })
+    );
+    console.log("B", bookings)
+
+  };
 
   return (
     <div>
       <h1>Make a Booking</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Name</label>
         <input
           type="text"
+          value={name}
+          onChange={handleNameChange}
           name="name"
           placeholder="Name"
         />
@@ -19,6 +59,8 @@ const ManageBookings = () => {
         <label>Last Name</label>
         <input
           type="text"
+          value={lastName}
+          onChange={handleLastChange}
           name="last"
           placeholder="Last Name"
         />
@@ -27,12 +69,33 @@ const ManageBookings = () => {
         <label>Date</label>
         <input
           type="date"
+          value={date}
+          onChange={handleDateChange}
           name="date"
           placeholder="Enter your dates"
         />
-      </form>
 
-      <CardView />
+        <button onClick={addBookings}>
+          submit
+        </button>
+      </form>
+      {console.log("Bookings", bookings)}
+      
+      {(bookings.length === 0) ? "loading....."
+      
+      : bookings.map((booking) => (
+      
+        // <div>
+        //   <h2>{booking.name}</h2>
+        //   <h2>{booking.lastName}</h2>
+        //   <h2>{booking.date}</h2>
+        // </div>
+      
+    
+      <CardView 
+      booking={booking}
+      />
+      ))}
     </div>
   );
 };
