117748c0532c36b416b9daeb7d1652afc9984805
your Shubinder Virk <shubinder92@gmail.com>
2020-07-30 14:47:57 +0100

    Styling for larger screens

--- client/src/Containers/Navbar/Navbar.styles.jsx
+++ client/src/Containers/Navbar/Navbar.styles.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Container = styled.div`
   position: sticky;
@@ -11,35 +11,33 @@ export const Container = styled.div`
 h3{
   font-size:10px;
 }
+.navbar{
+   background: linear-gradient(143deg, black 28%,#fdcd3b 70% ,#FFF 70%);
 
- background: linear-gradient(110deg, #fdcd3b 60%, #ffed4b 60%);.navbar {
   /* background: black; */
 }
-`
-
+ }
+`;
 
 export const Img = styled.img`
-width: 100px;
-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 1);
-border-radius:10px;
-`
+  width: 100px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 1);
+  border-radius: 10px;
+`;
 
 export const NavLinks = styled.div`
-padding-left:10px;
+  padding-left: 10px;
 
-Nav-Links{
-  text-align:right;
-}
-.booking {
-    margin-right:100px;
-    h2{
-        padding-left:100px;
+  Nav-Links {
+    text-align: right;
+  }
+  .booking {
+    margin-right: 100px;
+    h2 {
+      padding-left: 100px;
     }
-}
-`
-
-
-
+  }
+`;
 
 // Navbar Style
 // background: linear-gradient(143deg, black 28%,#fdcd3b 70% ,#FFF 70%);
