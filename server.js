ba6d92a78fc41f0bf67cb29fe4ae3594bb8b838c
your Shubinder Virk <shubinder92@gmail.com>
2020-08-16 14:10:52 +0100

    Deploying

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
   bodyParser.urlencoded({ extended: false })
 );
 app.use(bodyParser.json());
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(
     path.join(
       __dirname,
